7fbd38eed272ae34218114b09608202931cddb25
Alexis Giupponi <alexisgiupponi@gmail.com>
2013-05-08 23:16:58 -0300

    tp1

--- src/main/java/edu/tallerweb/pptls/Mano.java
+++ src/main/java/edu/tallerweb/pptls/Mano.java
@@ -10,8 +10,16 @@ public class Mano {
 	 * la que determine su condición en el juego.
 	 * @param forma, la Forma que adopta la Mano.
 	 */
+	private final Forma forma;
+	
 	public Mano(final Forma forma) {
-		throw new RuntimeException("No implementado aún");
+		
+		this.forma = forma;
+
+		if(forma == null) {
+				
+			throw new RuntimeException("No implementado aún");
+		}
 	}
 
 	/**
@@ -21,7 +29,65 @@ public class Mano {
 	 * @return un Resultado, de acuerdo al estado del juego.
 	 */
 	public Resultado jugarCon(final Mano otra) {
-		throw new RuntimeException("No implementado aún");
+		
+		Integer manoUno = this.forma.getValor();
+		Integer manoDos = otra.forma.getValor();
+		
+		/**
+		 * Piedra le gana a lagarto y a tijera
+		 */
+		if((manoUno == 0) && (manoDos > 2) ) {
+
+			return Resultado.GANA;
+
+		}
+		
+		/**
+		 * Spock le gana a piedra y a tijera
+		 */
+		if((manoUno == 1) && (manoDos == 0 || manoDos == 4)) {
+
+			return Resultado.GANA;
+
+		}
+		
+		/**
+		 * Papel le gana a a piedra y a Spock
+		 */
+		if((manoUno == 2) && (manoDos < 2)) {
+
+			return Resultado.GANA;
+
+		}
+		
+		/**
+		 * Lagarto gana a Spock y a papel
+		 */
+		if((manoUno == 3) && (manoDos == 1 || manoDos == 2)) {
+
+			return Resultado.GANA;
+
+		}
+		
+		/**
+		 * Tijera le gana a papel y a lagarto
+		 */
+		if((manoUno == 4) && (manoDos == 2 || manoDos == 3)) {
+
+			return Resultado.GANA;
+
+		}
+		
+		/**
+		 * En caso de empate.
+		 */
+		if(this.forma.equals(otra.forma)) {
+
+			return Resultado.EMPATA;
+
+		}
+
+		return Resultado.PIERDE;
 	}
 
 }
